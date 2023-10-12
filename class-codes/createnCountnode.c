4de124930bc86ec78e9237e7b16e75a4dab27adf
RAJJIT <2.0lairaj@gmail.com>
2023-10-12 07:27:26 +0530

    changes8.4

--- class-codes/createnCountnode.c
+++ class-codes/createnCountnode.c
@@ -7,46 +7,46 @@ struct node {
     struct node *next;
 };
 
+void insert(struct node **head, int val) {
+    struct node *newNode, *temp;
+    newNode = (struct node *)malloc(sizeof(struct node));
+    newNode->data = val;
+    newNode->next = NULL;
+    if (*head == NULL) {
+        *head = newNode;
+    } else {
+        temp = *head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+void displayCount(struct node *head) {
+    int count = 0;
+    struct node *temp = head;
+    printf("\nThe linked list is: ");
+    while (temp != NULL) {
+        printf("%d | ", temp->data);
+        temp = temp->next;
+        count++;
+    }
+    printf("\nThe number of nodes created is %d\n", count);
+}
+
 int main() {
-    int n, val;
+    int val;
     struct node *head = NULL;
-    int nodeCount = 0;
-
-    printf("Program for creation of a linked list and displaying the number of nodes created\n");
-
+    printf("Enter values for the list (enter -1 to finish):\n");
     while (1) {
-        printf("Enter a value (or -1 to stop): ");
         scanf("%d", &val);
         if (val == -1) {
             break;
         }
-        struct node *newNode = (struct node *)malloc(sizeof(struct node));
-        if (newNode == NULL) {
-            printf("Memory allocation failed. Exiting.\n");
-            return 1;
-        }
-        newNode->data = val;
-        newNode->next = NULL;
-
-        if (head == NULL) {
-            head = newNode;
-        } else {
-            struct node *temp = head;
-            while (temp->next != NULL) {
-                temp = temp->next;
-            }
-            temp->next = newNode;
-        }
-        nodeCount++;
+        insert(&head, val);
     }
-
-    printf("The linked list has %d nodes.\n", nodeCount);
-    printf("The elements in the linked list are: ");
-    struct node *temp = head;
-    while (temp != NULL) {
-        printf("%d | ", temp->data);
-        temp = temp->next;
-    }
-
+    displayCount(head);
     return 0;
 }
+
