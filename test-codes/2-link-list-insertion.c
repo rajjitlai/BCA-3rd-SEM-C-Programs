7a8b342f5f26c44a959d5b548148dec990033b0d
RAJJIT <2.0lairaj@gmail.com>
2023-10-17 08:26:03 +0530

    changes9.4

---
+++ test-codes/2-link-list-insertion.c
@@ -0,0 +1,7 @@
+// Program to create a link list and insert elements at the beginning and at any position
+
+/* 
+	You can check out the practical codes and the class codes for this program. The link is in the Readme file
+	Make sure to read the file before going to the links
+	:)
+*/
