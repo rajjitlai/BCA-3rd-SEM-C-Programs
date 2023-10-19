18082cf375f464295fac3c02aedb5d3369e2fe60
RAJJIT <2.0lairaj@gmail.com>
2023-10-19 23:22:08 +0530

    9.9-changed-search

--- practical-codes/link-list-item-search.c
+++ practical-codes/link-list-item-search.c
@@ -6,74 +6,68 @@ struct node {
     struct node *next;
 };
 
-struct node *createNode(int val) {
-    struct node *newNode = (struct node *)malloc(sizeof(struct node));
-    if (newNode == NULL) {
-        printf("Memory allocation failed. Exiting.\n");
-        exit(1);
-    }
-    newNode->data = val;
-    newNode->next = NULL;
-    return newNode;
+void insertElement(struct node **head, int val) {
+	struct node *newNode = (struct node *)malloc(sizeof(struct node));
+	newNode->data = val;
+	newNode->next = NULL;
+
+	if (*head == NULL) {
+		*head = newNode;
+	} else {
+		struct node *temp = *head;
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+		temp->next = newNode;
+	}
 }
 
-void insertAtEnd(struct node **head, int val) {
-    struct node *newNode = createNode(val);
-    if (*head == NULL) {
-        *head = newNode;
-    } else {
-        struct node *temp = *head;
-        while (temp->next != NULL) {
-            temp = temp->next;
-        }
-        temp->next = newNode;
-    }
+void searchElement(struct node **head, int val){
+	if(*head == NULL){
+		printf("The list is empty");
+	}else{
+		struct node *temp = *head;
+		int f = 0;
+		int pos = 1;
+		while (temp != NULL){
+			if(val == temp -> data)
+			{
+				f++;
+				break;
+			}
+			temp = temp -> next;
+			pos++;
+		}
+		if(f != 0){
+			printf("Element %d found at %d", val, pos);
+		}else{
+			printf("Element not found...");
+		}
+	}
 }
 
-int search(struct node *head, int val) {
-    struct node *temp = head;
-    int position = 1;
-    while (temp != NULL) {
-        if (temp->data == val) {
-            printf("Element %d found at position %d.\n", val, position);
-            return 1;
-        }
-        temp = temp->next;
-        position++;
-    }
-    printf("Element %d not found in the list.\n", val);
-    return 0;
+void printElement(struct node *head) {
+	struct node *temp = head;
+	while (temp != NULL) {
+		printf("%d | ", temp->data);
+		temp = temp->next;
+	}
 }
 
-void display(struct node *head) {
-    struct node *temp = head;
-    printf("Linked List: ");
-    while (temp != NULL) {
-        printf("%d | ", temp->data);
-        temp = temp->next;
-    }
-    printf("\n");
-}
-
-int main() {
-    int val;
-    struct node *head = NULL;
-
-    printf("Enter values for the list (enter -1 to finish):\n");
-    
-    while (1) {
-        printf("Enter a value: ");
-        scanf("%d", &val);
-        
-        if (val == -1) {
-            break;
-        }
-        
-        insertAtEnd(&head, val);
-    }
-    display(head);
-    printf("Enter a value to search in the list: ");
-    scanf("%d", &val);
-    search(head, val);
-    return 0;
+int main(){
+	int i, n, val, item;
+	struct node *head = NULL;
+	printf("Enter the number of elements you want to insert: ");
+	scanf("%d", &n);
+	for (i = 0; i < n; i++) {
+		printf("Enter the value for element %d: ", i + 1);
+		scanf("%d", &val);
+		insertElement(&head, val);
+	}
+	printf("The elements in the list are: ");
+	printElement(head);
+	printf("\nEnter the element you want to search: ");
+	scanf("%d", &item);
+	searchElement(&head, item);
+	return 0;
 }
