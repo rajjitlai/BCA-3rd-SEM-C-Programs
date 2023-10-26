e2ac831eb5865370d11ea7064502e83414dc5a41
RAJJIT <2.0lairaj@gmail.com>
2023-10-26 21:05:51 +0530

    10.7-queue-fixed

--- class-codes/queue-ins-del.c
+++ class-codes/queue-ins-del.c
@@ -1,17 +1,40 @@
 // Program to implment insertion and deletion in Queue
 #include<stdio.h>
 #include<stdlib.h>
-void enqueue(int arr[], int rear, int n) {
+int enqueue(int arr[], int rear, int n) {
 	if(rear == n - 1)
-		printf("Queue full");
+		printf("Queue is full");
 	else {
 		rear++;
 		printf("Enter the array data: ");
 		scanf("%d", &arr[rear]);
 	}
+	return rear;
 }
 
-void dequeue(int arr[], int front, int rear) {
+int dequeue(int arr[], int front, int rear) {
+	if(front == rear)
+		printf("Queue empty");
+	else {
+		front++;
+		printf("The deleted item is %d", arr[front]);
+	}
+	return front;
+}
+
+void disqueue(int arr[], int front, int rear) {
+	if (front == rear)
+		printf("Queue is empty\n");
+	else {
+		printf("\nQueue elements: ");
+		for (int i = front + 1; i <= rear; i++) {
+			printf("%d |", arr[i]);
+		}
+		printf("\n");
+	}
+}
+
+void peekq(int arr[], int front, int rear) {
 	if(front == rear)
 		printf("Queue empty");
 	else {
@@ -20,47 +43,33 @@ void dequeue(int arr[], int front, int rear) {
 	}
 }
 
-void display(int arr[], int front, int rear) {
-	if (front == rear)
-		printf("Queue is empty\n");
-	else {
-		printf("Queue elements: ");
-		for (int i = front + 1; i <= rear; i++) {
-			printf("%d ", arr[i]);
-		}
-		printf("\n");
-	}
-}
-
 int main() {
-	int a[50], num, rear = -1, front = -1, choice;
+	int a[20], num, rear = -1, front = -1;
 	printf("Enter the number of elements: ");
 	scanf("%d", &num);
-	if (num > 50)
-		printf("Queue size cannot exceed 50.\n");
+	if (num > 20)
+		printf("Queue size cannot exceed 20.\n");
 	else {
-		do {
-			printf("1. Enqueue\n2. Dequeue\n3. Display\n4. Exit\n");
-			printf("Enter your choice: ");
-			scanf("%d", &choice);
-			switch (choice) {
-				case 1:
-					enqueue(a, rear, num);
-					break;
-				case 2:
-					dequeue(a, front, rear);
-					break;
-				case 3:
-					display(a, front, rear);
-					break;
-				case 4:
-					printf("Exiting...\n");
-					exit(0);
-				default:
-					printf("Invalid choice. Try again.\n");
-			}
-		} while (choice != 4);
+		rear = enqueue(a, rear, num);
+		rear = enqueue(a, rear, num);
+		rear = enqueue(a, rear, num);
+		disqueue(a, front, rear);
+		front = dequeue(a, front, rear);
+		disqueue(a, front, rear);
+		peekq(a, front, rear);
 	}
 	return 0;
 }
-// there are some error that needs to be fixed
+
+// Output:
+/*
+Enter the number of elements: 5
+Enter the array data: 21
+Enter the array data: 24
+Enter the array data: 56
+
+Queue elements: 21 |24 |56 |
+The deleted item is 21
+Queue elements: 24 |56 |
+The deleted item is 24
+*/
