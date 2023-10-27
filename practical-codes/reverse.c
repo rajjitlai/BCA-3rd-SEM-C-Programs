626154d92a4ff3ee2c0b7db68c00dfb3cc335855
RAJJIT <2.0lairaj@gmail.com>
2023-10-27 22:16:01 +0530

    10.9-added-reverse

---
+++ practical-codes/reverse.c
@@ -0,0 +1,62 @@
+// Program to reverse a linked list
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node {
+	int data;
+	struct node *next;
+};
+
+void insertElement(struct node **head, int val) {
+	struct node *newNode = (struct node *)malloc(sizeof(struct node));
+	newNode->data = val;
+	newNode->next = NULL;
+
+	if (*head == NULL) {
+		*head = newNode;
+	} else {
+		struct node *temp = *head;
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+		temp->next = newNode;
+	}
+}
+
+void printElement(struct node *head) {
+	struct node *temp = head;
+	while (temp != NULL) {
+		printf("%d | ", temp->data);
+		temp = temp->next;
+	}
+}
+
+void printElementReverse(struct node *head) {
+	if (head == NULL) {
+		return;
+	}
+	printElementReverse(head->next);
+	printf("%d | ", head->data);
+}
+
+int main() {
+	int n, i, val;
+	struct node *head = NULL;
+	printf("Enter the number of elements you want to insert: ");
+	scanf("%d", &n);
+
+	for (i = 0; i < n; i++) {
+		printf("Enter the value for element %d: ", i + 1);
+		scanf("%d", &val);
+		insertElement(&head, val);
+	}
+
+	printf("The elements in the list are: ");
+	printElement(head);
+	
+	printf("\nReversed List: ");
+	printElementReverse(head);
+	printf("\n");
+
+	return 0;
+}
