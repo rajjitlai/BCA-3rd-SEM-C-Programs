6e6e71144992feaee7ffe938e00b54f1804fe0fe
RAJJIT <2.0lairaj@gmail.com>
2023-10-27 22:58:36 +0530

    11.3-added-circular-enqueue

--- class-codes/queue-ins-del-circular.c
+++ class-codes/queue-ins-del-circular.c
@@ -1,38 +1,57 @@
 // Program to implment enqueue in circular queue
 #include <stdio.h>
+int rear = -1;
+int front = -1;
 
-int enqueue(int arr[], int rear, int front, int n) {
-    if ((rear + 1) % n == front) {
-        printf("Queue is full\n");
-    } else {
-        rear = (rear + 1) % n;
-        printf("Enter the data: ");
-        scanf("%d", &arr[rear]);
-    }
-    return rear;
+void enqueueCir(int arr[], int n){
+	if((rear == -1) && (front == -1))
+	{
+		rear = front = 0;
+		printf("Enter the data: ");
+		scanf("%d", &arr[rear]);
+	}
+	else if (front == ((rear + 1) % n)){
+		printf("Queue is full");
+	}
+	else{
+		rear = (rear + 1) % n;
+		printf("Enter the data: ");
+		scanf("%d", &arr[rear]);
+	}
 }
 
-void displayQueue(int arr[], int front, int rear, int n) {
-    if (front == rear) {
-        printf("Queue is empty\n");
-    } else {
-        printf("Queue elements: ");
-        int i = front;
-        do {
-            printf("%d | ", arr[i]);
-            i = (i + 1) % n;
-        } while (i != (rear + 1) % n);
-        printf("\n");
-    }
+void displayQueue(int arr[], int n) {
+    if((rear == -1) && (front == -1)){
+    	printf("Queue is empty");
+	}
+	else{
+		int i = front;
+		printf("\nQueue elements");
+		while(i != rear){
+			printf(" %d | ", arr[i]);
+			i = (i + 1) % n;
+		}
+		printf(" %d", arr[rear]);
+	}
 }
 
 int main() {
-    int a[20], num, rear = -1, front = -1;
+    int a[20], num;
     printf("Enter the number of elements: ");
     scanf("%d", &num);
-    while (rear < num - 1) {
-        rear = enqueue(a, rear, front, num);
-    }
-    displayQueue(a, front, rear, num);
+    enqueueCir(a, num);
+    enqueueCir(a, num);
+    enqueueCir(a, num);
+    displayQueue(a, num);
     return 0;
 }
+
+// Output:
+/*
+Enter the number of elements: 5
+Enter the data: 1
+Enter the data: 2
+Enter the data: 3
+
+Queue elements 1 |  2 |  3
+*/
