1abda8bf27bb622b77a8048d45c92b32869936ba
RAJJIT <2.0lairaj@gmail.com>
2023-11-25 11:41:23 +0530

    14.4-fixed-bub-sort

--- practical-codes/bubbleSort.c
+++ practical-codes/bubbleSort.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 int main()
 {
     int arr[10];
@@ -11,28 +10,36 @@ int main()
     {
         scanf("%d", &arr[i]);
     }
+
     for (i = 0; i < n; i++)
     {
-        for (j = 0; j < n - 1 - i; i++)
+        flag = 0;
+        for (j = 0; j < n - 1 - i; j++)
         {
-            flag = 0;
             if (arr[j] > arr[j + 1])
             {
                 flag = 1;
-            }
-            if (flag == 1)
-            {
                 temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
             }
         }
+        if (flag == 0)
+        {
+            break;
+        }
     }
-    printf("Sorted list is: \n");
 
+    printf("Sorted list is: \n");
     for (i = 0; i < n; i++)
     {
         printf("%d ", arr[i]);
     }
     return 0;
 }
+
+// Output:
+// Enter the number of elements: 5
+// Enter the elements: 44 21 90 10 67
+// Sorted list is:
+// 10 21 44 67 90
