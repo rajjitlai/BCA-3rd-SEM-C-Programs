e5f384d4afc8dfe953ae7b811f44dd0e1274b647
RAJJIT <2.0lairaj@gmail.com>
2023-11-26 23:27:01 +0530

    14.6-changed-GCD-rec

--- practical-codes/recGCD.c
+++ practical-codes/recGCD.c
@@ -1,22 +1,14 @@
 #include <stdio.h>
-
-int gcd(int a, int b);
-
+int gcd(int a, int b) {
+    if (b == 0)
+        return a;
+    else
+        return gcd(b, a % b);
+}
 int main() {
     int num1, num2;
-
     printf("Enter two numbers: ");
     scanf("%d %d", &num1, &num2);
-
     printf("GCD of %d and %d is %d\n", num1, num2, gcd(num1, num2));
-
     return 0;
-}
-
-int gcd(int a, int b) {
-    if (b == 0) {
-        return a;
-    } else {
-        return gcd(b, a % b);
-    }
 }
